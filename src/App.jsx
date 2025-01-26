c432ce5d7cd48f0b07b7b08483eceff628284bf9
Igor <igor.olshanskyi@gmail.com>
2025-01-26 19:09:22 +0200

    solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
@@ -9,36 +9,40 @@ export const tabs = [
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-export const App = () => (
-  <div className="section">
-    <h1 className="title">Selected tab is Tab 1</h1>
+export const App = () => {
+  const [select, setSelect] = useState('');
+  const [selectNumb, setSelectNumb] = useState('');
 
-    <div data-cy="TabsComponent">
-      <div className="tabs is-boxed">
-        <ul>
-          <li className="is-active" data-cy="Tab">
-            <a href="#tab-1" data-cy="TabLink">
-              Tab 1
-            </a>
-          </li>
+  const handleClick = item => {
+    setSelect(item.content);
+    setSelectNumb(item.title);
+  };
 
-          <li data-cy="Tab">
-            <a href="#tab-2" data-cy="TabLink">
-              Tab 2
-            </a>
-          </li>
+  return (
+    <div className="section">
+      <h1 className="title">{`Selected tab is ${selectNumb}`}</h1>
 
-          <li data-cy="Tab">
-            <a href="#tab-3" data-cy="TabLink">
-              Tab 3
-            </a>
-          </li>
-        </ul>
-      </div>
+      <div data-cy="TabsComponent">
+        <div className="tabs is-boxed">
+          <ul>
+            {tabs.map(item => (
+              <li className="is-active" data-cy="Tab" key={item.id}>
+                <a
+                  href={`#${item.id}`}
+                  data-cy="TabLink"
+                  onClick={() => handleClick(item)}
+                >
+                  {item.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
 
-      <div className="block" data-cy="TabContent">
-        Some text 1
+        <div className="block" data-cy="TabContent">
+          {select}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
